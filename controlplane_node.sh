b0a9df6c7080e7be2bd89a17b377937519e5bf2a
unknown <khoj@osci.kr>
2024-05-14 21:49:14 +0900

    edu

--- controlplane_node.sh
+++ controlplane_node.sh
@@ -23,17 +23,17 @@ echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
 # git clone k8s-code
-git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git
-mv /home/vagrant/_Lecture_k8s_learning.kit $HOME
-find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+git clone https://github.com/osci-khoj/2024_k8s.git
+mv /home/vagrant/2024_k8s  $HOME
+find $HOME/2024_k8s -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 
 # make rerepo-k8s-learning.kit and put permission
-cat <<EOF > /usr/local/bin/rerepo-k8s-learning.kit
+cat <<EOF > /usr/local/bin/2024_k8s
 #!/usr/bin/env bash
-git clone https://github.com/dncs0725/2024_k8s.git $HOME/edu
-find $HOME/edu -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+git clone https://github.com/dncs0725/2024_k8s.git $HOME/2024_k8s
+find $HOME/2024_k8s -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 EOF
-chmod 700 /usr/local/bin/rerepo-k8s-learning.kit
+chmod 700 /usr/local/bin/2024_k8s
 
 ##### Addtional configuration for All-in-one >> replace to extra-k8s-pkgs
 
